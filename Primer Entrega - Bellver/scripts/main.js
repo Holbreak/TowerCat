e6d91a1e3e4fcc4a99864f75250ffceeb010892d
Joaco Bellver <143137139+Holbreak@users.noreply.github.com>
2023-09-07 15:57:25 -0300

    Add files via upload

--- Primer Entrega - Bellver/scripts/main.js
+++ Primer Entrega - Bellver/scripts/main.js
@@ -1,6 +1,7 @@
 let vidaTorre = 150;
 let round = 0;
 let gatoEnLaCelda = true
+let nombreDelJugador = "";
 
 const MAX_DAMAGE = 20;
 const MIN_DAMAGE = 5;
@@ -8,9 +9,9 @@ const MIN_DAMAGE = 5;
 const torre = document.getElementById("torre");
 const liberado = document.getElementById("liberado");
 
-function atacarTorre(damage) {
+atacarTorre = (damage) => {
     vidaTorre -= damage;
-    round+=1;
+    round += 1;
     console.log("Estas en el round " + round)
     if (vidaTorre <= 0) {
         vidaTorre = 0;
@@ -19,20 +20,51 @@ function atacarTorre(damage) {
         document.getElementById("gatito").style.display = "none";
         liberado.style.display = "block";
         console.log("Felicitaciones!! Liberaste al gatito!")
+        reiniciar();
     }
+    console.log(vidaTorre)
     let opacity = vidaTorre / 150;
     torre.style.opacity = opacity;
     document.getElementById("vidaTorreDisplay").textContent = vidaTorre;
-    
+
 }
 
-function atacarTorreClick() {
-    if (vidaTorre > 0){
+atacarTorreClick = () => {
+    if (vidaTorre > 0) {
         const damage = Math.ceil(Math.random() * (MAX_DAMAGE - MIN_DAMAGE)) + MIN_DAMAGE;
         atacarTorre(damage);
-        console.log(vidaTorre);
     }
 }
+
+reiniciar = () => {
+    if (vidaTorre === 0) {
+        document.getElementById("restart").style.visibility = "visible";
+    }
+    document.getElementById("restart").addEventListener("click", () => {
+        vidaTorre = 150;
+        let opacity = vidaTorre / 150;
+        torre.style.opacity = opacity;
+        document.getElementById("vidaTorreDisplay").textContent = vidaTorre;
+        document.getElementById("restart").style.visibility = "hidden";
+
+        document.getElementById("jaula").style.display = "block";
+        document.getElementById("gatito").style.display = "block";
+        liberado.style.display = "none";
+    });
+};
+
+while (nombreDelJugador === "" || nombreDelJugador === null) {
+    nombreDelJugador = prompt("Porfavor insertar nombre del usuario:");
+    if (nombreDelJugador === null || nombreDelJugador.trim() === "") {
+        alert("ingresa un nombre de usuario valido");
+        nombreDelJugador = "";
+    }
+}
+
+console.log("Bienvenido, " + nombreDelJugador);
+
+const usuarioSpan = document.getElementById("nombreUsuario");
+usuarioSpan.textContent = nombreDelJugador;
 
 const tipoArma = document.querySelectorAll(".arma");
 tipoArma.forEach(function (tipoArma) {
