76d1710e13e378c8b12d06cea87960a8c649f457
Joaco Bellver <143137139+Holbreak@users.noreply.github.com>
2023-09-28 19:21:44 -0300

    Add files via upload

--- Primer Entrega - Bellver/scripts/main.js
+++ Primer Entrega - Bellver/scripts/main.js
@@ -2,27 +2,55 @@ let vidaTorre = 150;
 let round = 0;
 let gatoEnLaCelda = true
 let nombreDelJugador = "";
+const mapasObjeto = {
+    jungle: {
+        nombre: "Jungla",
+        imagen: "assets/Fondo.png"
+    },
+    tunnel: {
+        nombre: "Tunel",
+        imagen: "assets/Fondo2.png"
+    },
+    city: {
+        nombre: "Ciudad",
+        imagen: "assets/Fondo3.png"
+    }
+};
+
+const torre = document.getElementById("torre");
+const liberado = document.getElementById("liberado");
+const searchInput = document.getElementById("searchInput");
+const listaArmas = document.getElementById("listaArmas");
+const dropdownMapas = document.getElementById("Mapas");
 
 const MAX_DAMAGE = 20;
 const MIN_DAMAGE = 5;
 
-const torre = document.getElementById("torre");
-const liberado = document.getElementById("liberado");
+dropdownMapas.addEventListener("change", function() {
+    const nivelSeleccionado = dropdownMapas.value;
+
+    const fondoSeleccionado = mapasObjeto[nivelSeleccionado];
+
+    if (fondoSeleccionado && fondoSeleccionado.imagen) {
+        fondo.src = `${fondoSeleccionado.imagen}`;
+        console.log("Cambiaste el nivel a " + fondoSeleccionado.nombre)
+    }
+});
 
 atacarTorre = (damage) => {
     vidaTorre -= damage;
     round += 1;
-    console.log("Estas en el round " + round)
+    console.log("Estas en el round " + round);
     if (vidaTorre <= 0) {
         vidaTorre = 0;
         gatoEnLaCelda = false;
         document.getElementById("jaula").style.display = "none";
         document.getElementById("gatito").style.display = "none";
         liberado.style.display = "block";
-        console.log("Felicitaciones!! Liberaste al gatito!")
+        console.log("Felicitaciones!! Liberaste al gatito!");
         reiniciar();
     }
-    console.log(vidaTorre)
+    console.log(vidaTorre);
     let opacity = vidaTorre / 150;
     torre.style.opacity = opacity;
     document.getElementById("vidaTorreDisplay").textContent = vidaTorre;
@@ -46,12 +74,16 @@ reiniciar = () => {
         torre.style.opacity = opacity;
         document.getElementById("vidaTorreDisplay").textContent = vidaTorre;
         document.getElementById("restart").style.visibility = "hidden";
-
         document.getElementById("jaula").style.display = "block";
         document.getElementById("gatito").style.display = "block";
         liberado.style.display = "none";
     });
 };
+
+const tipoArma = document.querySelectorAll(".arma");
+tipoArma.forEach(function (tipoArma) {
+    tipoArma.addEventListener("click", atacarTorreClick);
+});
 
 while (nombreDelJugador === "" || nombreDelJugador === null) {
     nombreDelJugador = prompt("Porfavor insertar nombre del usuario:");
@@ -65,8 +97,3 @@ console.log("Bienvenido, " + nombreDelJugador);
 
 const usuarioSpan = document.getElementById("nombreUsuario");
 usuarioSpan.textContent = nombreDelJugador;
-
-const tipoArma = document.querySelectorAll(".arma");
-tipoArma.forEach(function (tipoArma) {
-    tipoArma.addEventListener("click", atacarTorreClick);
-});
---
+++ Primer Entrega - Bellver/scripts/push.js
@@ -0,0 +1,82 @@
+const botones = document.querySelector("#armamento");
+const armasJugador = [
+    {
+        nombre: "Espada",
+        damage: 20,
+        imagen: "assets/Sword.png"
+    },
+    {
+        nombre: "Escopeta",
+        damage: 44,
+        imagen: "assets/Shotgun.png"
+    },
+    {
+        nombre: "Rifle",
+        damage: 65,
+        imagen: "assets/Rifle.png"
+    }
+]
+
+crearElemento = (tag, props, childs, text) => {
+    const element = document.createElement(tag)
+    if (props) {
+        Object.assign(element, props)
+    }
+    if (childs) {
+        childs.forEach( i => {
+            element.appendChild(i)
+        })
+    }
+    if (text) {
+        element.textContent = text;
+    }
+    return element
+}
+
+crearBoton = (nombre, imagen) => {
+    const btnTexto = crearElemento("strong", null, null, nombre)
+    const btnImg = crearElemento("img", {src: imagen} )
+    const btn = crearElemento("button",{className: "arma attackButton"}, [btnTexto, btnImg])
+    btn.addEventListener("click", ()=> atacarTorreClick())
+    return btn;
+} 
+
+mostrarSeleccionador = () => {
+    if (document.getElementById("armasInventory").style.display = "none") {
+        document.getElementById("armasInventory").style.display = "block"
+    }
+}
+
+const botonPlus = document.querySelector(".push")
+botonPlus.addEventListener("click", ()=> mostrarSeleccionador())
+
+imprimirArma = (nombre, damage, imagen ) => {
+    const item = crearElemento('li', null, null, `Arma: ${nombre}, Daño: ${damage}`)
+    const imgElement = crearElemento('img', { src: imagen });
+    item.classList.add('box-nuevaArma');
+    item.appendChild(imgElement);
+    item.addEventListener("click", ()=>{
+        if (botones.childElementCount < 5) {
+            const btn = crearBoton(nombre, imagen)
+            botones.appendChild(btn)
+            alert("Agregaste " + nombre + " a tu arsenal!")
+        }
+    })
+    return item;
+}
+
+mostrarArmas = () => {
+    const textoLower = searchInput.value.toLowerCase();
+    let armas = armasJugador;
+    if (textoLower) {
+        armas = armasJugador.filter(espada => espada.nombre.toLowerCase().includes(textoLower));
+    }
+        listaArmas.innerHTML = "";
+        armas.forEach(espada => {
+            const listItem = imprimirArma(espada.nombre, espada.damage, espada.imagen)
+            listaArmas.appendChild(listItem);
+    });
+}
+searchInput.addEventListener("input", mostrarArmas);
+mostrarArmas();
+
